3915dacfa66f622792b08ca95b821b891d93e519
Tgpeti <tgpeti@gmail.com>
2023-04-02 18:52:56 +0200

    ticketClosingApp id querries start

--- MindigFenyesTicketClosingApp/Program.cs
+++ MindigFenyesTicketClosingApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using MindigFenyesDB.Data;
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using MindigFenyesDB.Data;
+using MindigFenyesDB.Models;
+using System.Security.Claims;
 
 namespace MindigFenyesTicketClosingApp
 {
@@ -6,9 +9,28 @@ namespace MindigFenyesTicketClosingApp
     {
         static void Main(string[] args)
         {
-            using (var context = new MindigFenyesContext())
-            
+            var context = new MindigFenyesContext();
+
             Console.WriteLine("kérlek add meg az azonosítószámod");
+            int workerId = 0;
+            List<Worker> workers =context.Workers.ToList();
+            while (!int.TryParse(Console.ReadLine(), out workerId) && workers.SingleOrDefault(m => m.Id == workerId,null) is null)
+            {
+                Console.WriteLine("Rossz azonosítót adtál meg, kérlek próbáld újra!");
+            }
+            var worker = context.Workers.SingleOrDefault(m => m.Id == workerId);
+            Console.WriteLine($"Kedves {worker!.Name}, add meg az általad elvégzett javítás sorszámát!");
+            int ticketId = -1;
+            while (ticketId != 0)
+            {
+                List<Ticket> unfinishedTickets = context.Tickets.Where(t => t.IsFinished ==false).ToList();
+                while (!int.TryParse(Console.ReadLine(), out ticketId) && unfinishedTickets.SingleOrDefault(m => m.Id == ticketId, null) is null)
+                {
+                    Console.WriteLine("Rossz sorszámot adtál meg, kérlek próbáld újra!");
+                }
+                Console.WriteLine("kérlek add meg a meghibásodás okát! (izzó, foglalat, vezeték, egyéb)");
+                //while(Console.ReadLine() )
+            }
 
         }
     }
